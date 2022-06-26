15fed47a604d4ee480dc8104969adbf0f857ccc1
xXG4brielgit config --global user.name xXG4brielgit config --global user.name xXG4briel <gabrielgeorge95@gmail.com>
2022-06-26 17:08:01 -0300

    aula 3 - consumindo requests http e rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,16 @@
-import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react"
-import { Header } from "./components/Header";
-import { Lesson } from "./components/Lesson";
-import { SideBar } from "./components/SideBar";
-import { Video } from "./components/Video";
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 import { client } from "./lib/apollo";
-import { Event } from "./pages/Event";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-      teacher {
-        name
-      }
-    }
-  }
-`;
-
-interface Lesson {
-  id: string,
-  title: string
-}
+import { Router } from "./Router";
 
 function App() {
-  //  useEffect( () => {
-  //   client.query({
-  //     query: GET_LESSONS_QUERY
-  //   }).then( response => {
-  //     console.log(response);
-  //   })
-  //  }, []);
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);
   return (
-    <Event/>
-    // <div>
-    //   <SideBar></SideBar>,
-    //   <Video></Video>,
-    //   <Lesson></Lesson>
-    // </div>
-  )
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
+    )
 }
 
 export default App
--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,26 @@
+import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { SideBar } from "../components/SideBar";
 import { Video } from "../components/Video";
 
 export function Event (){
+
+    const { slug } = useParams<{slug: string}>();
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header/>
             <main className="flex flex-1">
-                <Video/>
+                { slug ? 
+                <Video lessonSlug={slug}/> : 
+                <div className="flex-1 flex flex-col w-full justify-center items-center group">
+                    <img className="w-[30%] max-w-[320px] opacity-80 group-hover:opacity-100" draggable="false" src="/src/assets/icons/the_search.svg" alt="Não encontrado" />
+                    <div className="w-[50%] max-w-[270px] flex flex-col justify-center items-center">
+                        <strong className="font-bold text-2xl text-gray-200 mt-4 mb-2">Ops !</strong>
+                        <span className="text-sm text-gray-200 text-center leading-relaxed">Não foi possível encontrar nenhum vídeo ... Tente selecionar outro vídeo ou tente novamente mais tarde.</span>
+                    </div>
+                </div> 
+                }
                 <SideBar/>
             </main>
         </div>
